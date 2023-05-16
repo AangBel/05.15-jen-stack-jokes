122d3aed5348274e8d7546d5c4ef9ac51bbabea0
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-15 22:52:04 -0500

    i need to learn more about post- im currently getting a cannot post/server error and i cant recall right now how to fix it

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,127 @@
-console.log('client.js sourced');
+console.log("client.js sourced");
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
+
+let who = $("#whoseJokeIn").val();
+let question = $("#questionIn").val(); 
+let punch = $("#punchlineIn").val();
+let jokes = [];
+
+jokes.push[
+    {
+    who: who,
+    question: $("#questionIn").val(),
+    punch: $("#punchlineIn").val()
+}
+];
+
+console.log(jokes);
+
 
 function onReady() {
-    console.log('DOM ready');
+console.log("DOM ready");
+$("#addJokeButton").on("click", post2Server);
 }
+
+function post2Server(event) {
+console.log("post2Server is joking");
+
+event.preventDefault();
+
+$.ajax({
+    type: 'POST',
+    url: "/server",
+    // data: {
+    //     who: $("#whoseJokeIn").val(""),
+    //     question: $("#questionIn").val(""),
+    //     punch: $("#punchlineIn").val("")
+    // }
+}).then(function(response){
+    console.log('Ajax and shit is working so far!');
+    console.log(response);
+    renderToDom(response);
+})
+.catch(function (error) {
+    alert("ah shit catch-ed this thing wrong");
+    console.log("Request failed", error);
+});
+
+who = $("#whoseJokeIn").val();
+console.log(who);
+
+question = $("#questionIn").val();
+console.log(question);
+
+punch = $("#punchlineIn").val();
+console.log(punch);
+
+console.log(who, question, punch);
+
+   
+}
+// renderToDom();
+
+    // function getFromServer() {
+    //     $.ajax({
+    //     method: "GET",
+    //     url: "/server",
+    //     })
+    //     .then(function (response) {
+    //         console.log(response);
+    //         console.log("omg! GET from server is working?!");
+    //         renderToDom(response);
+    //     })
+    //     .catch(function (error) {
+    //         alert("request failed!, try again...");
+    //         console.log("request for GET failed!", error);
+    //     });
+    //     // calculator();
+    //   } //end of get from server
+
+
+function getFromArray() {
+    console.log("getFromArray is working");
+    $.ajax({
+    method: "GET",
+    url: "/server",
+    text: {
+        first: who,
+        second: question, 
+        third: punch,
+      }, //end of data
+    })
+    .then(function (response) {
+        console.log(response);
+        console.log("omg! GET from server is working?!");
+        renderToDom(response);
+    })
+    .catch(function (error) {
+        alert("request failed!, try again...");
+        console.log("request for GET failed!", error);
+    });
+    // calculator();
+}
+function renderToDom(jokes) {
+    console.log("Got Data!");
+
+
+    let wholeThing = (jokes[0].who + jokes[0].question + jokes[0].punch);
+    $("#outputDiv").append(`
+
+    <li>${wholeThing[jokes.length -1]}</li>
+    <li>${wholeThing}</li>
+    `);
+} 
+renderToDom();
+// $('#answer').text(total)
+
+
+    // let total = $(inputA) + potato + $(inputB);
+    // console.log("total", total);
+    // for (let i of calcHx{length-1});
+
+    
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,42 +1,70 @@
-const express = require( 'express' );
+const express = require("express");
 const app = express();
-const bodyParser = require( 'body-parser' );
+const bodyParser = require("body-parser");
 const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("server/public"));
+// app.use(express.static("/server"));
+//jokes is an array , that already holds within it some elements
+//we want to
 
 let jokes = [
   {
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat!"
+    punchLine: "If they fell forwards they’d still be in the boat!",
   },
   {
     whoseJoke: "Luke",
     jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
-    punchLine: "Do you know how to drive this thing?"
+    punchLine: "Do you know how to drive this thing?",
   },
   {
     whoseJoke: "Millie",
     jokeQuestion: "What do you call a pile of cats?",
-    punchLine: "A meowntain!"
+    punchLine: "A meowntain!",
   },
   {
     whoseJoke: "dEv",
     jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs!"
+    punchLine: "Too many Cheetahs!",
   },
   {
     whoseJoke: "Scott",
     jokeQuestion: "I went to the zoo the other day, it had one dog...",
-    punchLine: "It was a shih tzu."
-  }
+    punchLine: "It was a shih tzu.",
+  },
+  // for(let i of jokes{})
 ];
+console.log(jokes);
 
+app.get("/GET", function (req, res) {
+  console.log("Request for /GET was made");
+
+  // send back list of quotes to client
+  res.send(jokes[0]);
+
+  // If I want to send an error:
+  // res.sendStatus(500);
+});
+
+app.post("/POST", function (req, res) {
+  console.log("POST some data!", req.body);
+
+  req.body.whoseJoke;
+  req.body.jokeQuestion;
+  req.body.punchLine;
+  // let grabJokes = req.body;
+  // jokes.push({grabJokes})
+
+  // let saveJokes =  `${grabJokes.jokes}`
+  // console.log(saveJokes);
+});
 // serve back static files
-app.use(express.static('server/public'));
+
 
 app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
+  console.log("server running on: ", PORT);
 }); // end spin up server
